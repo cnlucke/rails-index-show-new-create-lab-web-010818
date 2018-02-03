8bb5c27b5c2b147a9acb58e31d83f2e9f373705a
cnlucke <cristy.lucke@gmail.com>
2018-02-03 14:22:12 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(name: "Avocado", coupon_code: "AVCD", store: "Westside Market")
+Coupon.create(name: "Apples", coupon_code: "APP", store: "Westside Market")
+Coupon.create(name: "Grapes", coupon_code: "GRPS", store: "Westside Market")
+Coupon.create(name: "Cheese", coupon_code: "CHSE", store: "Westside Market")
+Coupon.create(name: "Meat", coupon_code: "MT", store: "Westside Market")
+Coupon.create(name: "Bread", coupon_code: "BRD", store: "Westside Market")
